90102d88f9a40233980c6ccd3d343af56c5bf469
Sambit25bce11252 <sambit.25bce11252@vitbhopal.ac.in>
2025-11-22 00:12:16 +0530

    Added if/elif/else condition to staisfy user choices for Expense Tracker.

--- ExpenseTrackerCode.py
+++ ExpenseTrackerCode.py
@@ -10,3 +10,27 @@ while True:
     print("4. Exit")
 
     choice = input("Enter choice: ")        # Choose any appropriate option you want to execute.
+    
+    if choice == "1":                       # Condition to terminate if user chooses to add expenses.
+        name = input("Enter item name: ")
+        amount = float(input("Enter amount: "))
+        expenses.append({"item": name, "amount": amount})
+        print("Expenses updated.")
+
+    elif choice == "2":                     # Condition to terminate if user chooses to view expenses.
+        print("\n--- Expenses ---")
+        for i in range(len(expenses)):
+            print(str(i+1) + ". " + expenses[i]["item"] + " - " + str(expenses[i]["amount"]))
+
+    elif choice == "3":                     # Condition to terminate if user chooses to know total spent expenses.
+        total = 0
+        for i in expenses:
+            total += i["amount"]
+        print("Total spent = " + str(total))
+
+    elif choice == "4":                     # Condition to terminate if user wants to exit the loop.
+        print("Thank you")
+        break
+
+    else:
+        print("Invalid choice!")            # Condition to terminate if user enters an unexpected choice .
