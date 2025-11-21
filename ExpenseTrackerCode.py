095b7c8ba37bef24a21233966ca1fc393b41c5c2
Sambit25bce11252 <sambit.25bce11252@vitbhopal.ac.in>
2025-11-21 23:43:31 +0530

    Added the initial choices for the user.

---
+++ ExpenseTrackerCode.py
@@ -0,0 +1,12 @@
+# Mini Expense Tracker 
+
+expenses = []   # each entry = {"item": name, "amount": value}
+
+print("Choose one of the following option .")
+while True:
+    print("\n1. Add Expense")
+    print("2. View Expenses")
+    print("3. Total Spent")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")        # Choose any appropriate option you want to execute.
